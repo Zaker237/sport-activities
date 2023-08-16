8175f02360b0879c7497c7b69ab4d4ac6580f5e4
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-08-16 18:25:39 +0200

    initial commit

---
+++ strava.py
@@ -0,0 +1,36 @@
+# from __future__ import print_statement
+import time
+import os
+import json
+import requests
+#import swagger_client
+#from swagger_client.rest import ApiException
+from pprint import pprint
+
+from dotenv import load_dotenv
+load_dotenv()
+
+AUTH_URL = "https://www.strava.com/oauth/token"
+
+
+def get_auth():
+    payload = {
+        'client_id': os.environ.get("STRAVA_CLIENT_ID"),
+        'client_secret': os.environ.get("STRAVA_CLIENT_SECRET"),
+        'refresh_token': os.environ.get("STRAVA_REFRESH_TOKEN"),
+        'code': os.environ.get("STRAVA_CLIENT_CODE"),
+        'grant_type': "authorization_code"
+    }
+
+    response = requests.post(AUTH_URL, data=payload)
+    #Save json response as a variable
+    strava_tokens = response.json()
+    # Save tokens to file
+    with open('strava_tokens.json', 'w') as outfile:
+        json.dump(strava_tokens, outfile)
+    return response.json()
+
+
+
+if __name__ == "__main__":
+    get_auth()
